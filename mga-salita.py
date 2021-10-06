76774600cbcb77d6c332a4303179d4c6546bd38c
Marco Enrico <mvalviar@gmail.com>
2021-10-06 14:39:37 +0800

    Inalis ang python script.

--- mga-salita.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient;
-
-client = MongoClient('localhost')
-db = client.updf
-entries = db.entries.find({}, {'id': 1, '_id': 0}).sort([('order', 1)]) 
-for entry in entries:
-    print(entry['id'])
-
